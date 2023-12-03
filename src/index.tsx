111ecd07f9b85afb4f4dcb4ca193b5d0c974a4cc
aman2222kumar <amanda.asd444@gmail.com>
2023-12-03 15:49:10 +0530

    initial update

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     
-    <BrowserRouter>
+    <BrowserRouter basename='/crud-app-typescript-and-react'>
     <App />
     </BrowserRouter>
   </React.StrictMode>
